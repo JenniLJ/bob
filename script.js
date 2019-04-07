8669dbb606659641924254c6eb20caef2804c0c8
ajbinky <ajbinky@iastate.edu>
2019-04-06 22:53:27 -0500

    array of audio

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let arr = [];
 
 let a = new Audio('a.wav');
+arr.push(a);
+let b = new Audio('b.wav');
+arr.push(b);
+let c = new Audio('c.wav');
+arr.push(c);
+let d = new Audio('d.wav');
+arr.push(d);
+let e = new Audio('e.wav');
+arr.push(e);
+let f = new Audio('f.wav');
+arr.push(f);
+let g = new Audio('g.wav');
+arr.push(g);
 
 function fun() {
-	a.play();
+	for (let i = 0; i < arr.length; i++) {
+		arr[i].play();
+	}
 }
